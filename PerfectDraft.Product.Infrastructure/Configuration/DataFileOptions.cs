bd9c650e80a2a5aa5d6a2ed74d8dc4670decf60b
jsnso <jsnso@JSHOMELAPTOP>
2026-09-04 20:37:18 +0100

    Add Configuration IOption DI for the Data directory files where magenta and search product files live

---
+++ PerfectDraft.Product.Infrastructure/Configuration/DataFileOptions.cs
@@ -0,0 +1,9 @@
+namespace PerfectDraft.Product.Infrastructure.Configuration
+{
+    public class DataFileOptions
+    {
+        public required string MagentoProductsPath { get; set; }
+
+        public required string SearchProductsPath { get; set; }
+    }
+}
--- PerfectDraft.Product.Infrastructure/Configuration/ServiceConfiguration.cs
+++ PerfectDraft.Product.Infrastructure/Configuration/ServiceConfiguration.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PerfectDraft.Product.Infrastructure.Configuration;
+using PerfectDraft.Product.Infrastructure.Infrastructure;
 using PerfectDraft.Product.Infrastructure.Repository;
+using System.IO.Abstractions;
 
 namespace PerfectDraft.Product.Api.Configuration
 {
     public static class ServiceConfiguration
     {
-        public static void RegisterRepositories(this IServiceCollection Services)
+        public const string DataFilesKey = "DataFiles";
+
+        public static void RegisterRepositories(this IServiceCollection Services, IConfiguration Configuration)
         {
+            Services.Configure<DataFileOptions>(Configuration.GetSection(DataFilesKey));
+
+            Services.AddSingleton<IFileSystem, FileSystem>();
+            Services.AddSingleton<IJsonFileLoader, JsonFileLoader>();
             Services.AddSingleton<IProductRepository, ProductRepository>();
         }
     }

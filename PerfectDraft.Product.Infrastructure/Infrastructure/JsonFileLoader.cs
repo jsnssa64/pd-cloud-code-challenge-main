1cd262846874041cbfa507be1450ac9f0a1bdbcd
jsnso <jsnso@JSHOMELAPTOP>
2026-09-04 20:39:24 +0100

    Add json deserialization options to ignore casing when converting to class and add IFileSystem abstraction for File reading

--- PerfectDraft.Product.Infrastructure/Infrastructure/JsonFileLoader.cs
+++ PerfectDraft.Product.Infrastructure/Infrastructure/JsonFileLoader.cs
@@ -5,10 +5,12 @@ namespace PerfectDraft.Product.Infrastructure.Infrastructure
 {
     public class JsonFileLoader(IFileSystem FileSystem) : IJsonFileLoader
     {
+        private readonly JsonSerializerOptions Options = new() { PropertyNameCaseInsensitive = true };
+
         public async Task<IEnumerable<T>> ReadAllJsonFileAsync<T>(string path, CancellationToken cancellationToken)
         {
             using var stream = FileSystem.File.OpenRead(path);
-            return await JsonSerializer.DeserializeAsync<IEnumerable<T>>(stream, cancellationToken: cancellationToken)
+            return await JsonSerializer.DeserializeAsync<IEnumerable<T>>(stream, Options, cancellationToken)
                 ?? throw new InvalidOperationException($"Unable to Deserialize '{path}' to {typeof(T)}");
         }
     }

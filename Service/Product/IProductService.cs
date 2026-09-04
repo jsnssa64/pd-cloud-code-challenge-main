5439dad9303aa356d93e16ec854460c65479b8a5
jsnso <jsnso@JSHOMELAPTOP>
2026-09-04 20:43:27 +0100

    Add DTO building with defaulting to Search Product, Make effecient Get Products by Parallel Process, Add Multi search term checking and returning list of Products rather then one

--- Service/Product/IProductService.cs
+++ Service/Product/IProductService.cs
@@ -5,5 +5,5 @@ namespace PerfectDraft.Product.Service.Product;
 public interface IProductService
 {
     Task<ProductDTO?> GetProduct(ProductSkuDTO sku, CancellationToken cancellationToken);
-    Task<ProductDTO?> SearchProduct(ProductSearchTermDTO searchTerm, CancellationToken cancellationToken);
+    Task<IEnumerable<ProductDTO>> SearchProduct(ProductSearchTermDTO searchTerm, CancellationToken cancellationToken);
 }
--- Service/Product/ProductService.cs
+++ Service/Product/ProductService.cs
@@ -1,41 +1,68 @@
-﻿using Microsoft.Extensions.Logging;
-using PerfectDraft.Product.Infrastructure.Repository;
+﻿using PerfectDraft.Product.Infrastructure.Repository;
 using PerfectDraft.Product.Shared.DTO;
 
 namespace PerfectDraft.Product.Service.Product;
-public class ProductService(ILogger<ProductService> Logger, IProductRepository ProductRepository) : IProductService
+public class ProductService(IProductRepository ProductRepository) : IProductService
 {
     public async Task<ProductDTO?> GetProduct(ProductSkuDTO Sku, CancellationToken cancellationToken)
     {
-        try
-        {
-            var products = await ProductRepository.GetProducts(cancellationToken);
-            var searchProducts = await ProductRepository.GetSearchProducts(cancellationToken);
+        var getProducts = ProductRepository.GetProducts(cancellationToken);
+        var searchProducts = ProductRepository.GetSearchProducts(cancellationToken);
 
-        }
-        catch (Exception ex)
+        await Task.WhenAll(getProducts, searchProducts);
+
+        var product = getProducts.Result.FirstOrDefault(product => string.Equals(product.Sku, Sku.Sku, StringComparison.OrdinalIgnoreCase));
+
+        if (product is null)
         {
-            //  Do not Block just Return empty object
-            Logger.LogError(ex, "");
+            return null;
         }
 
-        return null;
+        var productMetaData = searchProducts.Result.FirstOrDefault(sproduct => string.Equals(sproduct.Sku, product.Sku, StringComparison.Ordinal));
+
+        return new ProductDTO(
+            new ProductSkuDTO(product.Sku),
+            product.Name,
+            productMetaData?.Title ?? product.Name,
+            productMetaData?.Image ?? "http://default.com/default.png",
+            product.Price,
+            product.Currency,
+            product.Stock,
+            (product.Stock > 0)
+            );
     }
 
-    public async Task<ProductDTO?> SearchProduct(ProductSearchTermDTO searchTerm, CancellationToken cancellationToken)
+    public async Task<IEnumerable<ProductDTO>> SearchProduct(ProductSearchTermDTO searchTerm, CancellationToken cancellationToken)
     {
-        try
-        {
-            var products = await ProductRepository.GetProducts(cancellationToken);
-            var searchProducts = await ProductRepository.GetSearchProducts(cancellationToken);
+     
+        var getProducts = ProductRepository.GetProducts(cancellationToken);
+        var searchProducts = ProductRepository.GetSearchProducts(cancellationToken);
 
-        }
-        catch (Exception ex)
-        {
-            //  Do not Block just Return empty object
-            Logger.LogError(ex, "");
-        }
+        await Task.WhenAll(getProducts, searchProducts);
 
-        return null;
+
+        var searchTerms = searchTerm.Search.Split(' ', StringSplitOptions.TrimEntries);
+
+        var filteredProducts = getProducts.Result
+            .Where(product => searchTerms.Any(term => product.Name.Contains(term, StringComparison.OrdinalIgnoreCase)));
+
+        var searchProductsResults = await searchProducts;
+
+        return filteredProducts.Select(product =>
+        {
+            var productMetaData = searchProductsResults.FirstOrDefault(sproduct => string.Equals(sproduct.Sku, product.Sku, StringComparison.Ordinal));
+            
+            return new ProductDTO(
+                new ProductSkuDTO(product.Sku),
+                product.Name,
+                productMetaData?.Title ?? product.Name,
+                //  Default to a default Image
+                productMetaData?.Image ?? "http://default.com/default.png",
+                product.Price,
+                product.Currency,
+                product.Stock,
+                (product.Stock > 0)
+            );
+        });
     }
 }
